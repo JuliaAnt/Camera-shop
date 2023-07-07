9b2753ada87b0545b5949e0889b944cb68ba2683
Julia Antoshkina <julia.antoshkina@gmail.com>
2023-07-07 20:23:58 +0200

    Fixed search

--- src/components/search/search-result-item/search-result-item.tsx
+++ src/components/search/search-result-item/search-result-item.tsx
@@ -1,5 +1,6 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { ProductCard } from '../../../types/product-card';
+import { KeyboardEvent } from 'react';
 
 type SearchResultItemProps = {
   product: ProductCard;
@@ -7,9 +8,24 @@ type SearchResultItemProps = {
 }
 
 function SearchResultItem({ product, searchString }: SearchResultItemProps): JSX.Element {
+  const navigate = useNavigate();
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLLIElement>) => {
+    if (event.key === 'Enter') {
+      navigate(`/camera/${product.id}?tab=description`);
+    }
+  };
+
   return (
     <Link to={`/camera/${product.id}?tab=description`}>
-      <li className="form-search__select-item" tabIndex={0} style={{ visibility: `${searchString ? 'visible' : 'hidden'}` }}>{product.name}</li>
+      <li
+        className="form-search__select-item"
+        id={'search'}
+        tabIndex={1}
+        style={{ visibility: `${searchString ? 'visible' : 'hidden'}` }}
+        onKeyDown={handleKeyDown}
+      >{product.name}
+      </li>
     </Link>
   );
 }
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from '../../hooks/redux-hooks';
 import { getProducts } from '../../store/catalog-data/catalog-data-selectors';
 import SearchResultItem from './search-result-item/search-result-item';
 import { ProductCard } from '../../types/product-card';
+import FocusTrap from 'react-focus-trap';
 
 function Search(): JSX.Element {
   const products = useAppSelector(getProducts);
@@ -11,37 +12,48 @@ function Search(): JSX.Element {
 
   const onChange = (evt: ChangeEvent<HTMLInputElement>) => {
     setSearchString(evt.target.value.toLowerCase());
-    setSearchResultsList(products.filter((product) => {
-
-      if (evt.target.value) {
-        return product.name.toLowerCase().includes(evt.target.value.toLowerCase());
-      }
-      return product.name.toLowerCase().includes('блаблабла');
+    if (evt.target.value === '') {
+      return setSearchResultsList([]);
     }
-    ));
+    setSearchResultsList(products.filter((product) => product.name.toLowerCase().includes(evt.target.value.toLowerCase())));
+  };
+
+  const onResetClick = () => {
+    setSearchString('');
   };
 
   return (
     <div className="form-search" data-testid={'form-search'}>
-      <form>
-        <label>
-          <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-lens"></use>
-          </svg>
-          <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" onChange={onChange} />
-        </label>
-        <ul
-          className="form-search__select-list scroller"
-          style={{ visibility: `${searchResultsList.length !== 0 && searchString ? 'visible' : 'hidden'}`, opacity: `${searchResultsList.length !== 0 && searchString ? 1 : 0}` }}
-        >
-          {searchResultsList.map((result) => <SearchResultItem key={result.id} product={result} searchString={searchString} />)}
-        </ul>
-      </form>
-      <button className="form-search__reset" type="reset">
-        <svg width="10" height="10" aria-hidden="true">
-          <use xlinkHref="#icon-close"></use>
-        </svg><span className="visually-hidden">Сбросить поиск</span>
-      </button>
+      {/* @ts-expect-error children */}
+      <FocusTrap
+        focusTrapOptions={{
+          initialFocus: '#search',
+          isKeyForward: (event: KeyboardEvent) => event.key === 'ArrowDown',
+          isKeyBackward: (event: KeyboardEvent) => event.key === 'ArrowUp',
+          clickOutsideDeactivates: true,
+        }}
+      >
+        <form>
+          <label>
+            <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
+              <use xlinkHref="#icon-lens"></use>
+            </svg>
+            <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" value={searchString} onChange={onChange} />
+          </label>
+          <ul
+            className="form-search__select-list scroller"
+            style={{ visibility: `${searchResultsList.length !== 0 && searchString ? 'visible' : 'hidden'}`, opacity: `${searchResultsList.length !== 0 && searchString ? 1 : 0}` }}
+          >
+            {searchResultsList.map((result) => <SearchResultItem key={result.id} product={result} searchString={searchString} />)}
+          </ul>
+
+        </form>
+        <button className="form-search__reset" type="button" style={{ display: `${searchString ? 'flex' : 'none'}` }} onClick={onResetClick}>
+          <svg width="10" height="10" aria-hidden="true">
+            <use xlinkHref="#icon-close"></use>
+          </svg><span className="visually-hidden">Сбросить поиск</span>
+        </button>
+      </FocusTrap>
     </div>
   );
 }
